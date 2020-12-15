3637373d1126d01b6671889211e67bed59f892a2
tt1pjm <48781860+tt1pjm@users.noreply.github.com>
2020-12-15 14:42:00 +0000

    Commit from GitHub Actions

--- docs/lvgl.js
+++ docs/lvgl.js
@@ -1152,9 +1152,9 @@ function updateGlobalBufferAndViews(buf) {
   Module['HEAPF64'] = HEAPF64 = new Float64Array(buf);
 }
 
-var STACK_BASE = 5857120,
+var STACK_BASE = 5857232,
     STACKTOP = STACK_BASE,
-    STACK_MAX = 614240;
+    STACK_MAX = 614352;
 
 assert(STACK_BASE % 16 === 0, 'stack must start aligned');
 
